198a28b380f6d20119ab3056b40c595de58480d4
sir-meee <sammymbevi@gmail.com>
2021-04-29 14:43:37 +0300

    query cities from database

--- weather/views.py
+++ weather/views.py
@@ -1,21 +1,27 @@
 import requests
 from django.shortcuts import render
+from .models import City
 
 # Create your views here.
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=48f737f5bee28dbe93b34fbb92f67ff2'
     city = 'Nairobi'
 
-    r = requests.get(url.format(city)).json()
-    # print(r.text)
+    cities = City.objects.all()
+    weather_data = []
+    for city in cities:
+        r = requests.get(url.format(city)).json()
+        # print(r.text)
 
-    city_weather = {
-        'city': city,
-        'temperature': r['main']['temp'],
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon'],
-    }
-    # print(city_weather)
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+        # print(city_weather)
+        weather_data.append(city_weather)
+    # print(weather_data)
 
-    context = {'city_weather': city_weather}
+    context = {'weather_data': weather_data}
     return render(request, 'weather/weather.html', context)
